8d7e84f84c8b0e8d57e98c35a1206df78328bc9d
BuildTools <unconfigured@null.spigotmc.org>
2019-05-21 08:23:25 +0800

    main and TCP code added

---
+++ main.py
@@ -0,0 +1,13 @@
+from mcts.Board import Board
+from mcts import MonteCarloTreeSearch
+
+import STcpClient
+
+
+def main():
+    # TODO: get state and return actions
+    pass
+
+
+if __name__ == '__main__':
+    main()
